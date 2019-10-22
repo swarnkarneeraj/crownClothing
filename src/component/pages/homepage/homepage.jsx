0a3c76f93d3f41c59ddae4b35bdfd0264a5ee85c
swarnkarneeraj <sneeraj.itech@gmail.com>
2019-10-23 02:20:52 +0530

    Components and all

---
+++ src/component/pages/homepage/homepage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./homepage.scss";
+
+import Directory from "../../directory/directory";
+
+export const HomePage = () => (
+  <div className="homepage">
+    <Directory></Directory>
+  </div>
+);
