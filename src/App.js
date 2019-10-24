22494d7ada5f5042d7c4b6babf0db319285db785
swarnkarneeraj <sneeraj.itech@gmail.com>
2019-10-24 20:18:54 +0530

    implement header and logo

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import { HomePage } from "./component/pages/homepage/homepage";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./component/shop/shop";
+import Header from "./component/header/header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route exact path="/shop" component={ShopPage}></Route>
